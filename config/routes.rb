35078324ba2401b8745f265950918358c4b58277
mustanser ali <razaali199913@outlook.com>
2023-05-23 14:24:56 +0200

    todo task

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/tasks', to: 'tasks#index'
+  post '/tasks', to: 'tasks#create'
+  get 'tasks/new', to: 'tasks#new'
+  get 'task/:id/edit', to: 'tasks#edit', as: 'tasks_edit'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
